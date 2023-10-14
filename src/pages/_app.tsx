9ea080cb1837f9c2d2103c861329584cc64bb46f
Yale Araujo <yale850@gmail.com>
2023-10-14 11:17:07 -0300

    refactor: removing unused content and setting default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['700', '600', '500', '400'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={poppins.className} {...pageProps} />
 }
