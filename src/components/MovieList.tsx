66a451cce42e4e85b68d9808585fe8d25b7c7b3b
Yale Araujo <yale850@gmail.com>
2023-12-15 16:54:21 -0300

    feat: new movie list component

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,27 @@
+import { isEmpty } from 'lodash'
+import { MovieCard } from './MovieCard'
+
+interface MovieListProps {
+  data: Record<string, unknown>[]
+  title: string
+}
+
+export function MovieList({ data, title }: MovieListProps) {
+  if (isEmpty(data)) return null
+
+  return (
+    <div className="px-4 md:px-12 mt-4 space-y-8">
+      <div>
+        <p className="text-white text-base md:text-xl lg:text-2xl font-semibold mb-4">
+          {title}
+        </p>
+
+        <div className="grid grid-cols-4 gap-2">
+          {data?.map((movie) => (
+            <MovieCard key={movie.id as string} data={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
