fde16239341974119984e90ef394318c991335c0
Yale Araujo <yale850@gmail.com>
2023-12-16 03:43:14 -0300

    feat: billboard component

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -1,5 +1,6 @@
 import { useBillboard } from '@/hooks/useBillboard'
 import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { PlayButton } from './PlayButton'
 
 export function Billboard() {
   const { data } = useBillboard()
@@ -24,6 +25,7 @@ export function Billboard() {
         </p>
 
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <PlayButton movieId={data?.id} />
           <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center gap-1 hover:bg-opacity-20 transition">
             <AiOutlineInfoCircle />
             More Info
